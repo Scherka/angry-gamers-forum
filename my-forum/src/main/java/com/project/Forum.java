d370d06195309048479fedfb298dcf2baed14cbc
Scherka <s.cher.a@mail.ru>
2025-04-17 23:28:10 +0400

    Initial commit

---
+++ my-forum/src/main/java/com/project/Forum.java
@@ -0,0 +1,12 @@
+// src/main/java/com/project/Forum.java
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forum {
+    public static void main(String[] args) {
+        SpringApplication.run(Forum.class, args);
+    }
+}
