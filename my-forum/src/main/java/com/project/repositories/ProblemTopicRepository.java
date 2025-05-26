a6e7a77566170a63d385a8aba8a67e7884a03ca6
s.cher.@mail.ru <s.cher.@mail.ru>
2025-05-26 21:43:54 +0400

    Репозиториям добавлены методы existsBy... и findBy...

--- my-forum/src/main/java/com/project/repositories/ProblemTopicRepository.java
+++ my-forum/src/main/java/com/project/repositories/ProblemTopicRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProblemTopicRepository extends JpaRepository<ProblemTopic, Long> {
+    boolean existsByName(String name);
 }
